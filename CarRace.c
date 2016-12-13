978afed54ee6b0f5ebe6c1e835b9f05f6f520c84
Thomas <thomulusparker@gmail.com>
2016-12-12 18:51:38 -0800

    final commit

--- CarRace.c
+++ CarRace.c
@@ -5,19 +5,15 @@
 #include<sys/time.h>
 
 int notdone = 1;
-int car1 = 0;
-int car2 = 0;
-int car3 = 0;
-int car4 = 0;
-int car5 = 0;
+int cars[5];
 pthread_t threads[6];
 
 void *humanwaiting(void *threadid){
 	while(notdone){
 		char enter = getchar();
 		if (enter == '\n'){
-			car1 +=1;
-			if (car1 ==40){
+			cars[1] +=1;
+			if (cars[1] >=40){
 				notdone = 0;
 			}
 		}
@@ -26,12 +22,27 @@ void *humanwaiting(void *threadid){
 	}
 }
 void *drawing(void *threadid){
-	printf("hello");
 	while(notdone){
-		printf("hello");
-		usleep(1000);
-		printf("HELLO EVERYONE %d",car1);
+		FILE * f = fopen("temp.txt","w");
+		for (int car = 1; car < 6; car++){
+			for (int i = 0; i < cars[car]; i++){
+				fprintf(f,"~");
+			}
+			fprintf(f,"|->");
+			for (int i = 0; i < 40- cars[car]; i++){
+				fprintf(f," ");
+			}
+			fprintf(f,"# Lane %d #\n", car);
+		}
+		fclose(f);
+		system("clear");
+		system("echo 'Welcome to CISC220 Racing Arena'");
+		system("echo 'Hit Enter to move forward\n'");
+		system ("cat temp.txt");
 		usleep(200000);
+		//int rc;
+		//rc = pthread_create(threads+1,NULL,humanwaiting,(void*)tid);
+		system("rm temp.txt");
 	}
 }
 void *waiting(void *threadid){
@@ -41,28 +52,15 @@ void *waiting(void *threadid){
 	waittime = (waittime/RAND_MAX)*1000000;
 	usleep(waittime);
 	//printf("%ld Waited %Le microseconds\n",tid, waittime);
-	switch (tid){
-		case 1:
-			car1+=1;
-			break;
-		case 2:
-			car2+=1;
-			break;
-		case 3:
-			car3+=1;
-			break;
-		case 4:
-			car4+=1;
-			break;
-		case 5:
-			car5+=1;
-			break;
-	}
-	if ((car1 <40) & (car2<40) & (car3<40) & (car4<40) & (car5<40)){
+	cars[tid]+=1;
+	if ((cars[1] <40) & (cars[2]<40) & (cars[3]<40) & (cars[4]<40) & (cars[5]<40)){
 		int rc = pthread_create(threads+tid,NULL,waiting,(void*)tid);
 	}
 	else{
 		notdone = 0;
+		for (int i =1; i <6; i ++){
+			pthread_cancel(threads[i]);
+		}
 	}
 	pthread_exit(NULL);
 }
@@ -89,6 +87,12 @@ int main(){
 	}
 	while (notdone){
 	}
-	printf("car1: %d, car2: %d, car3: %d, car4: %d, car5: %d",car1,car2,car3,car4,car5);
+	rc = pthread_create(threads,NULL,drawing, (void*)0);
+	printf("car1: %d, car2: %d, car3: %d, car4: %d, car5: %d\n",cars[1],cars[2],cars[3],cars[4],cars[5]);
+	for (int i =1; i<6;i++){
+		if (cars[i] >= 40){
+			printf("Player %d WINS!", i);
+		}
+	}
 	pthread_exit(NULL);
 }
